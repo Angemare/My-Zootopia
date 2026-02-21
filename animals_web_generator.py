e8da3c9d69c935cb3ec5418bdb991a8d74a055e7
angemare <a.stragies.com>
2026-02-21 17:01:42 +0100

    Key Error solved - access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,15 +9,42 @@ def load_data(file_path):
 
 # print list - nested structure
 animals_data = load_data('animals_data.json')
-print(animals_data)
-print(type(animals_data))
+#print(animals_data)
+#print(type(animals_data))
 
-# Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE
+# Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE (if given)
 for animal in animals_data:
-    print("Name:", animal['name'])
-    print("Diet:", animal['characteristics']['diet'])
-    print("Location:", animal['locations'][0])
-    print("Type:", animal['characteristics']['type'])
+
+    name = animal.get('name', {})
+    name_value = animal.get('name', 'unknown')
+    if name_value != 'unknown':
+        print("Name:", name_value)
+
+    characteristics = animal.get('characteristics', {})
+    diet_value = characteristics.get('diet', 'unknown')
+    if diet_value != 'unknown':
+        print("Diet:", diet_value)
+
+    locations = animal.get('locations', {})
+    locations_value = animal.get('locations', 'unknown')
+    if locations_value != 'unknown':
+        print("Location:", locations_value[0])
+
+    characteristics = animal.get('characteristics',{})
+    type_value = characteristics.get('type', 'unknown')
+    if type_value != 'unknown':
+        print("Type:", type_value)
+
+
+
+
+
+
+    #print("Name:", animal['name'])
+    #print("Diet:", animal['characteristics']['diet'])
+    #print("Location:", animal['locations'][0])
+
+    #print("Type:", animal['characteristics']['type'])
     print("")
 
 
