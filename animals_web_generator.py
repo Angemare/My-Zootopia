8d6bfc9fc3cc655726e11c18556fed8e9814033a
angemare <a.stragies.com>
2026-02-22 15:48:59 +0100

    Format Code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,8 +10,6 @@ def load_data(file_path):
 
 # print json list - nested structure
 animals_data = load_data('animals_data.json')
-#print(animals_data)
-#print(type(animals_data))
 
 # read content of the template html
 def read_html(file_path):
@@ -21,45 +19,42 @@ def read_html(file_path):
 
 # Create string - reading animals data from new html "animals.html"
 animals_template = read_html('animals_template.html')
-#print(animals_template)
 
-
-
-# create empty string
-output = ''
-
-# Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE (if given)
-for animal in animals_data:
-
+# Create a function
+def serialize_animal(animal_obj):
+    output = ''
     #append information to each string
     output += '<li class="cards__item">\n'
 
-    name = animal.get('name', {})
     name_value = animal.get('name', 'unknown')
     if name_value != 'unknown':
         output += f"<div class='card__title'> {name_value}</div><br/>\n"
-        #print("Name:", name_value)
+
     output += '<p class="card__text">'
+
     characteristics = animal.get('characteristics', {})
     diet_value = characteristics.get('diet', 'unknown')
     if diet_value != 'unknown':
         output += f"<strong>Diet:</strong> {diet_value}<br/>\n"
-        #print("Diet:", diet_value)
 
-    locations = animal.get('locations', {})
     locations_value = animal.get('locations', 'unknown')
     if locations_value != 'unknown':
         output += f"<strong>Location:</strong> {locations_value[0]}<br/>\n"
-        #print("Locations:", locations_value[0])
 
     characteristics = animal.get('characteristics',{})
     type_value = characteristics.get('type', 'unknown')
     if type_value != 'unknown':
         output += f"<strong>Type:</strong> {type_value}<br/>\n"
-    output += "</p></li>\n"
-        #print("Type:", type_value)
 
-print(output)
+    output += "</p></li>\n"
+
+    return output
+
+# create empty string
+output = ''
+# Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE (if given)
+for animal in animals_data:
+    output += serialize_animal(animal)
 
 # replace old html with new html
 new_html = animals_template.replace("__REPLACE_ANIMALS_INFO__", output )
