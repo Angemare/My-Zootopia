5e420f81822d0030b6d9b16c32d496f2f54eb4fd
angemare <a.stragies.com>
2026-02-22 14:13:42 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,16 +1,32 @@
 
-# Javascript Object notation
+# Javascript Object Notation
 import json
+
 # opens json file, with open r for read the file
 def load_data(file_path):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="UTF-8") as handle:
         return json.load(handle)
 
-# print list - nested structure
+# print json list - nested structure
 animals_data = load_data('animals_data.json')
 #print(animals_data)
 #print(type(animals_data))
+
+# read content of the template html
+def read_html(file_path):
+    """ loads html file """
+    with open(file_path, "r", encoding="UTF-8") as handle:
+        return handle.read()
+
+# Create string - reading animals data from new html "animals.html"
+animals_template = read_html('animals_template.html')
+#print(animals_template)
+
+
+
+# create empty string
+output = ''
 
 # Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE (if given)
 for animal in animals_data:
@@ -18,25 +34,37 @@ for animal in animals_data:
     name = animal.get('name', {})
     name_value = animal.get('name', 'unknown')
     if name_value != 'unknown':
-        print("Name:", name_value)
+        output += f"Name: {name_value}\n"
+        #print("Name:", name_value)
 
     characteristics = animal.get('characteristics', {})
     diet_value = characteristics.get('diet', 'unknown')
     if diet_value != 'unknown':
-        print("Diet:", diet_value)
+        output += f"Diet: {diet_value}\n"
+        #print("Diet:", diet_value)
 
     locations = animal.get('locations', {})
     locations_value = animal.get('locations', 'unknown')
     if locations_value != 'unknown':
-        print("Location:", locations_value[0])
+        output += f"Location: {locations_value[0]}\n"
+        #print("Locations:", locations_value[0])
 
     characteristics = animal.get('characteristics',{})
     type_value = characteristics.get('type', 'unknown')
     if type_value != 'unknown':
-        print("Type:", type_value)
+        output += f"Type: {type_value}\n"
+    output += "\n"
+        #print("Type:", type_value)
 
+print(output)
 
-    print("")
+# replace old html with new html
+new_html = animals_template.replace("__REPLACE_ANIMALS_INFO__", output )
+print(new_html)
+
+# write HTML
+with open("animals.html","w", encoding= "UTF-8") as handle:
+    handle.write(new_html)
 
 
 
