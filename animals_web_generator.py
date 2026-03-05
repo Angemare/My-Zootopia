9a16f7ddc9aa064cf47890f524a0e9e0be308922
angemare <a.stragies.com>
2026-03-05 13:19:51 +0100

    improve .py

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -26,22 +26,22 @@ def serialize_animal(animal_obj):
     #append information to each string
     output += '<li class="cards__item">\n'
 
-    name_value = animal.get('name', 'unknown')
+    name_value = animal_obj.get('name', 'unknown')
     if name_value != 'unknown':
         output += f"<div class='card__title'> {name_value}</div><br/>\n"
 
     output += '<p class="card__text">'
 
-    characteristics = animal.get('characteristics', {})
+    characteristics = animal_obj.get('characteristics', {})
     diet_value = characteristics.get('diet', 'unknown')
     if diet_value != 'unknown':
         output += f"<strong>Diet:</strong> {diet_value}<br/>\n"
 
-    locations_value = animal.get('locations', 'unknown')
+    locations_value = animal_obj.get('locations', 'unknown')
     if locations_value != 'unknown':
         output += f"<strong>Location:</strong> {locations_value[0]}<br/>\n"
 
-    characteristics = animal.get('characteristics',{})
+    characteristics = animal_obj.get('characteristics',{})
     type_value = characteristics.get('type', 'unknown')
     if type_value != 'unknown':
         output += f"<strong>Type:</strong> {type_value}<br/>\n"
@@ -50,6 +50,9 @@ def serialize_animal(animal_obj):
 
     return output
 
+
+def main():
+    """ main function - function call """
 # create empty string
 output = ''
 # Task - iterate thru list + print: NAME, DIET, 1st LOCATION, TYPE (if given)
@@ -60,10 +63,11 @@ for animal in animals_data:
 new_html = animals_template.replace("__REPLACE_ANIMALS_INFO__", output )
 print(new_html)
 
-# write HTML
+# write HTML in a function
 with open("animals.html","w", encoding="UTF-8") as handle:
     handle.write(new_html)
 
-
+if __name__ == "__main__":
+    main()
 
 
